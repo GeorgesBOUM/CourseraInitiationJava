5b7df9d3c271bd1b1cf40613dea0cc071440fba3
GregoireMaison <gregoire.maison@icloud.com>
2018-04-06 23:54:10 +0200

    updating TrancheMax

--- src/TrancheMax.java
+++ src/TrancheMax.java
@@ -35,95 +35,75 @@ class TrancheMax {
     /*******************************************
      * Completez le programme a partir d'ici.
      *******************************************/
-    
-    /*
-     * méthode emptyMatrix de verification de matrice vide 
-     */
-    public static boolean emptyMatrix(String matrix) {	
-    	String lines[] = matrix.split(" {1,}");
-    	int linesSize = lines.length;
-    	boolean empty;
-    	if (lines.length > 1) {
-    		int i = 1;
-    		do {
-    			if (lines[i].isEmpty()) {
-    				empty = true;
-    			} else {
-    				empty = false;
-    			}
-    			i++;
-    		} while ((!empty) && (i < lines.length));
-    	} else {
-			if (linesSize == 1) {
-				if (lines[0].isEmpty()) {
-					empty = true;
-				} else {
-					empty = false;
-				}
-			} else {
-				empty = true;
-			}
+   
+    /**
+    * method emptyMatrix : boolean
+    * tests whether the matrix
+    * is empty or not
+    */
+   public static boolean emptyMatrix(String matrix) {
+   	matrix = matrix.trim();
+   	String lines[] = matrix.split(" {1,}");
+   	
+   	if (lines[0].isEmpty()) {
+			//System.out.print("matrice vide");
+			return true;
+		} else {
+			return false;
 		}
-	return empty;
-    }
-    
-    /* La fonction checkLine
-	 * parcourt un tableau de string en vérifiant 
-	 * si les lignes ont la même longueur : TRUE  sinon FALSE
+   }
+   
+   /**
+    * method checkLine : boolean
+    * tests whether the elements of
+    * an array of String 
+    * have the same length or not
 	 */
 	public static boolean checkLine(String [] tab, int i) {
 		boolean sameLength;
-		do {
-			if (tab[i].length() == tab[i+1].length()) {
-				sameLength = true;
-			} else {
-				sameLength = false;
-			}
-			i++;
-		} while ((sameLength) && (i < (tab.length - 1)));
+		if (tab.length>1) {
+			do {
+				if (tab[i].length() == tab[i+1].length()) {
+					sameLength = true;
+				} else {
+					sameLength = false;
+				}
+				i++;
+			} while ((sameLength) && (i < (tab.length - 1)));
+		} else {
+			sameLength = true;
+		}
 		return sameLength;
 	}
-    
+   
 	
-    /*
-     * méthode checkLineLength de vérification de la taille des lignes de la matrice
-     */
+   /**
+    * method checkLineLength : boolean
+    * tests whether the lines of matrix of char
+    * have the same length or not
+    */
 	public static boolean checkLineLength(String matrix) {
 		boolean goodLength;
 		boolean empty = emptyMatrix(matrix);
-		int i = 0;
+		matrix = matrix.trim();
 		String [] lines = matrix.split(" {1,}");
+		int i = 0;
 		
 		if (empty) {
 			System.out.println("Matrice vide !");
 			goodLength = false;
+			return goodLength;
 		} else {
-			if (lines.length > 1) {
-				if (!lines[0].isEmpty()) {
-					goodLength = checkLine(lines, i);
-				} else {
-					i = 1;
-					if (lines.length == 2) {
-						goodLength = true;
-					} else {
-						goodLength = checkLine(lines, i);
-					}	
-				}
-			} else {
-				if (lines.length == 1) {
-					goodLength = true;
-				} else {
-					goodLength = false;
-				}
-			}
+			goodLength = checkLine(lines, i);
 		}
 		return goodLength;
 	}
-    
-	/* la fonction format : booléen
-	 * parcourt un tableau de string en vérifiant
-	 * qu'il ne contient que des '0' ou des '1' : TRUE
-	 * sinon : FALSE
+   
+	/**
+	 *  method format : boolean
+	 *  tests whether an array of String
+	 *  contains only 2 types of characters: 
+	 *  '0' or '1'
 	 */
 	public static boolean format (String [] tab, int i, int j) {
 		boolean formatCorrect = false;
@@ -138,129 +118,54 @@ class TrancheMax {
 				}
 			}
 		}
-		/* avec le do ... while
-		do {
-			do {
-				if ((tab[i].charAt(j) == '0') || (tab[i].charAt(j) == '1')) {
-					formatCorrect = true;
-				} else {
-					formatCorrect = false;
-					System.out.println("Matrice invalide : seuls '1', '0' et ' ' sont admis !");
-				}
-				j++;
-			} while ((formatCorrect) && (j < (tab[i].length() )));
-			i++;
-		} while ((formatCorrect) && (i < (tab.length )));
-		*/
 		return formatCorrect;
 	}
 	
-    /*
-     * méthode checkFormat de vérification de la conformité de la matrice
-     */
+   /**
+    * method checkFormat : boolean
+    * tests whether all the lines of a matrix
+    * have the same length and 
+    * contains only '0' or '1
+    */
 	public static boolean checkFormat(String matrix) {
 		boolean goodLength = checkLineLength(matrix);
 		boolean emptiness = emptyMatrix(matrix);
 		boolean goodFormat;
+		matrix = matrix.trim();
 	String [] lines = matrix.split(" {1,}");
-	int i, j, k; 
+	int i = 0, j = 0; 
 	
 	if (emptiness) {
 		goodFormat = false;
 		return goodFormat;
 	} else {
-		if (lines[0].isEmpty()) {
-			for (k = 1; k < lines.length; k++) {
-				for (int l = 0; l < lines[k].length(); l++) {
-					if (lines[k].charAt(l) != '0' && lines[k].charAt(l) != '1') {
-						goodFormat = false;
-						System.out.println("Matrice invalide : seuls '1', '0' et ' ' sont admis !");
-						return goodFormat;
-					}
-				}
-			}
-		} else {
-			for (k = 0; k < lines.length; k++) {
-				for (int l = 0; l < lines[k].length(); l++) {
-					if (lines[k].charAt(l) != '0' && lines[k].charAt(l) != '1') {
-						goodFormat = false;
-						System.out.println("Matrice invalide : seuls '1', '0' et ' ' sont admis !");
-						return goodFormat;
-					}
-				}
-			}
-		}
 		if (goodLength) {
-			if (lines.length > 1) {
-				if (!lines[0].isEmpty()) {
-					i = 0; j = 0;
-					goodFormat = format(lines, i, j);
-				} else {
-					i = 1; j = 0;
-					if (lines.length == 2) {
-						goodFormat = format(lines, i, j);
-					} else {
-						goodFormat = format(lines, i, j);
-					}
-				}
-			} else {
-				i = 0; j = 0;
-				goodFormat = format(lines, i, j);
-			}
+			goodFormat = format(lines, i, j);
+			return goodFormat;
 		} else {
 			System.out.println("Matrice invalide, lignes de longueurs differentes !");
 			goodFormat = false;
 			return goodFormat;
 		}
 	}
-	return goodFormat;
 }
-    
-    
-    /*
-     * la méthode findConsecutiveList qui renvoie dans un ArrayList
-     * les indices des lignes ayant le plus de '1'
-     */
-    
-	public static ArrayList<Integer> findConsecutiveList(String matrix) {
-    	ArrayList<Integer> maxList = new ArrayList<Integer>();
-    	int [][] eachLine;
-    	/** 
-    	 * le tableau eachLine sera une matrice de taille
-    	 * eachLineLength lignes et 2 colonnes
-    	 * chaque ligne contient 
-    	 * les indices 'réels' des lignes (du tableau lines)
-    	 * suivi dans la 2ème colonne du nombre de '1' par lignes (du tableau lines)
-    	 * les deux dimensions sont de même longueur eachLineLength 
-    	 */
-    	int eachLineLength;
-    	String lines [] = matrix.split(" {1,}");
-    	int compteur;
-    	if (lines[0].isEmpty()) {
-		eachLineLength = lines.length - 1;
-		eachLine = new int [eachLineLength][2];
-		for (int i = 1; i < lines.length; i++) {
-    		compteur = 0;
-			for (int j = 0; j < lines[i].length(); j++) {
-				if (lines[i].charAt(j) == '1') {
-					compteur++;
-				} else {
-					if (compteur > eachLine[i-1][1]) {
-						eachLine[i-1][1] = compteur;
-					}
-					compteur = 0;
-				}
-			}
-			eachLine[i-1][0] = i - 1;
-			if ((eachLine[i-1][1] < compteur) ) {
-				eachLine[i-1][1] = compteur;
-			}
-		}
-	} else {
-		eachLineLength = lines.length;
-		eachLine = new int [eachLineLength][2];
+   
+   
+   /**
+    * method consecutiveList : array of integer
+    * find the maximum number of 
+    * consecutive '1' in each line of a matrix
+    * eachLine[][] has 2 columns and in each line
+    * the index of the matrix line and the number of '1' 
+    */
+	
+	public static int [][] consecutiveList(String matrix) {
+		matrix = matrix.trim();
+		String [] lines = matrix.split(" {1,}");
+		int eachLine [][] = new int [lines.length][2];
+		int compteur;
 		for (int i = 0; i < lines.length; i++) {
-    		compteur = 0;
+   		compteur = 0;
 			for (int j = 0; j < lines[i].length(); j++) {
 				if (lines[i].charAt(j) == '1') {
 					compteur++;
@@ -276,16 +181,29 @@ class TrancheMax {
 				eachLine[i][1] = compteur;
 			}
 		}
+		return eachLine;
 	}
-    	
-    	if (eachLine != null) {
-    		for (int i = 0; i < eachLine.length; i++) {
-    			if ((maxList.isEmpty()) && (eachLine[i][1] > 0)) {
-				maxList.add(i);
+	
+	/**
+    * method findConsecutiveList : ArrayList
+    * listing the index of lines with the max consecutive '1'
+    */
+ 
+	public static ArrayList<Integer> findConsecutiveList(String matrix) {
+   	ArrayList<Integer> maxList = new ArrayList<Integer>();
+   	//listing the max number of consecutive '1' by line
+   	int [][] eachLine = consecutiveList(matrix);
+   	//listing the index of lines with the max consecutive '1'
+   	for (int i = 0; i < eachLine.length; i++) {
+			if ((maxList.isEmpty()) && (eachLine[i][1] > 0)) {
+			maxList.add(i);
+			if (eachLine.length>1) {
 				i++;
 			}
-    			
-    			if (!maxList.isEmpty() && (eachLine[maxList.get(maxList.size()-1)][1] > 0)) {
+		}
+			
+			if (eachLine.length>1) {
+				if (!maxList.isEmpty() && (eachLine[maxList.get(maxList.size()-1)][1] > 0)) {
 					if (eachLine[i][1] == eachLine[maxList.get(maxList.size()-1)][1]) {
 					maxList.add(i);
 				} else {
@@ -295,15 +213,16 @@ class TrancheMax {
 					}
 				}
 			}
-    		}
-    	}
-    	return maxList;
-    }
+			}
+	}
+   	return maxList;
+   }
 
-    //System.out.println("Matrice invalide, lignes de longueurs differentes !");
-    //System.out.println("Matrice vide !");
-    //System.out.println("Matrice invalide : seuls '1', '0' et ' ' sont admis !");
+   //System.out.println("Matrice invalide, lignes de longueurs differentes !");
+   //System.out.println("Matrice vide !");
+   //System.out.println("Matrice invalide : seuls '1', '0' et ' ' sont admis !");
 
+    
     /*******************************************
      * Ne rien modifier apres cette ligne
      *******************************************/
